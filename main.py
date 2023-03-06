9e5367d52f717ff8201213e8189d1cfd9e026bc8
saltyniggo <nico.schulz1337@gmail.com>
2023-03-06 14:30:57 +0100

    added the functions into main.py to increase performance

--- main.py
+++ main.py
@@ -1,14 +1,69 @@
 from sqlite3 import connect
 from random import randint
-from access import accessdata
-from input import inputdata
-from change import changedata
 from subprocess import call
 
 masterPassword = '1234'
 runToken = True
 accessAttempts = 0
 accessSucceeded = False
+
+
+def accessdata():
+    accountlogin = input("Which login data do you need?: ")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM passwords WHERE account = ?",
+                (accountlogin,))
+    rows = cur.fetchall()
+    if len(rows) == 0:
+        print("No matching login data found.")
+    else:
+        for row in rows:
+            call("clear", shell=True)
+            print(row)
+
+def changedata():
+    updateaccount = input("For which account do you want to change the password?: ")
+    call("clear", shell=True)
+    updatepassword = input("Please enter the new password: ")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM passwords WHERE account = ?",
+                (updateaccount,))
+    rows = cur.fetchall()
+    if len(rows) == 0:
+        print("No matching account found.")
+    else:
+        conn.execute("UPDATE passwords SET password = ? WHERE account = ?", (updatepassword, updateaccount))
+        conn.commit()
+        print("Data updated")
+
+
+def inputdata():
+    account = input("Enter account: ")
+    call("clear", shell=True)
+    website = input("Enter website: ")
+    call("clear", shell=True)
+    email = input("Enter email: ")
+    call("clear", shell=True)
+    username = input("Enter username: ")
+    call("clear", shell=True)
+    password = input("Enter password: ")
+    call("clear", shell=True)
+    if account and password and (email or username):
+        cur = conn.cursor()
+        cur.execute(
+            "SELECT * FROM passwords WHERE account = ? AND website = ? AND email = ? AND username = ? AND password = ?",
+            (account, website, email, username, password))
+        existing_data = cur.fetchone()
+        if existing_data:
+            print("Login data already exists for this account.")
+        else:
+            conn.execute("INSERT INTO passwords (account, website, email, username, password) VALUES (?, ?, ?, ?, ?)",
+                         (account, website, email, username, password))
+            conn.commit()
+            print("Login data added successfully.")
+    else:
+        print("Data missing, please try again")
+
 
 while runToken:
     if not accessSucceeded:
